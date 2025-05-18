aea247f0872309f8a59fa01293215df692db6e34
JuanMolfese <juanjosemolfese@gmail.com>
2025-05-17 22:53:55 -0300

    Importante: se actualiza codigo de google search consolese, se usa el cidog que genera desde el usuario dueño del perfil de google. Se agregan distintos iconos para resultados de busquedas de motores. se agregan etiquetas meta en el layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,15 +22,25 @@ export default function RootLayout({
   return (
     <html lang="es" className="light">
       <head>
-        <link rel="icon" href="/smallLogo3.png" type="image/png" />
+        <link rel="icon" href="/smallLogo3.png" type="image/png"/>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Yitani Propiedades" />
+        <link rel="manifest" href="/site.webmanifest" />
        {/*  <Script
           src={`https://maps.googleapis.com/maps/api/js?key=AIzaSyCicHfcmOSeaWlTIEIbV3gR2XkCeYn76bo&libraries=places`}
           strategy="beforeInteractive"
         /> */}
         <script async
           src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCicHfcmOSeaWlTIEIbV3gR2XkCeYn76bo&loading=async&libraries=places">
-        </script>
-        <meta name="google-site-verification" content="LuJasdcxHIafPuqyo8G19zM6IeVD_YBXrOQU-q4CvFQ" />
+        </script>        
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Encuentra las mejores propiedades en venta y alquiler en Tres Arroyos, Buenos Aires y zona" />
+        <meta name="keywords" content="inmobiliaria, propiedades, venta, alquiler, Tres Arroyos, Buenos Aires" />
+        <meta name="robots" content="content= index, follow" />
+        <meta name="google-site-verification" content="YJJsRFa8lUxdpa5_uQt1jcZsU8y6wAoEqxZ-ZBVo71w" />
       </head>
       <body className={inter.className}>
         {/* <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange> */}
